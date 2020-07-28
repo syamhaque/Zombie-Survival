f99c213d090d33dcf1f6a8d5e749858379f9d7ba
syamhaque <msyamhaque@gmail.com>
2020-07-28 00:32:00 -0700

    moved files

--- Zombie Survival/src/Workspace/Location/Barbed Wire Wall/Model/Model/Model/Barb Wire/Wire/Script.server.lua
+++
@@ -1,9 +0,0 @@
-function onTouched(part)
-	local h = part.Parent:findFirstChild("Humanoid")
-	if h~=nil then
-		h.Health = h.Health -10
-		wait(1)
-	end
-end
-
-script.Parent.Touched:connect(onTouched)
